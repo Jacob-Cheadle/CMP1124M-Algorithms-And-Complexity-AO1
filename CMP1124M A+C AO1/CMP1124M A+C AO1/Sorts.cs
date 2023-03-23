f17d1218c922fb5e438d3a296bbf69348914e810
Jacob-Cheadle <117683892+Jacob-Cheadle@users.noreply.github.com>
2023-03-23 11:17:24 +0000

    Implemented Final Testing & Commented Sorts.cs

--- CMP1124M A+C AO1/CMP1124M A+C AO1/Sorts.cs
+++ CMP1124M A+C AO1/CMP1124M A+C AO1/Sorts.cs
@@ -2,21 +2,22 @@
 {
     internal class Sorts
     {
+        //Function to run the Ascending Bubble Sort
         public static int[] BubbleSortAscending(int[] list)
         {
             var n = list.Length;
             for (int i = 0; i < n - 1; i++)
-                for (int j = 0; j < n - i - 1; j++)
+                for (int j = 0; j < n - i - 1; j++) //loops through the array a minimum of once making n comparisons
                     if (list[j] > list[j + 1])
                     {
                         var tempVar = list[j];
-                        list[j] = list[j + 1];
+                        list[j] = list[j + 1]; //compares 2 values, if one is bigger than the one infornt, they are swapped
                         list[j + 1] = tempVar;
                     }
             return list;
         }
 
-        public static int[] BubbleSortDescending(int[] list)
+        public static int[] BubbleSortDescending(int[] list) //same as ascending but with the for loop flipped so the values are set to the end of the array not the start
         {
             var n = list.Length;
             for (int i = n - 2; i >= 0; i--)
@@ -30,14 +31,14 @@
             return list;
         }
 
-        public static int[] InsertionSortAscending(int[] list)
+        public static int[] InsertionSortAscending(int[] list) //Ascending insertion sort
         {
             int length = list.Length;
             for (int i = 1; i < length; i++)
             {
                 var key = list[i];
                 var flag = 0;
-                for (int j = i - 1; j >= 0 && flag != 1;)
+                for (int j = i - 1; j >= 0 && flag != 1;) //runs through the array and finds a value, it then INSERTS the value in the correct position
                 {
                     if (key < list[j])
                     {
@@ -51,7 +52,7 @@
             return list;
         }
 
-        public static int[] InsertionSortDescending(int[] list)
+        public static int[] InsertionSortDescending(int[] list) //Same as ascending but again flipped values in the for loop
         {
             int length = list.Length;
             for (int i = 1; i < length; i++)
@@ -72,26 +73,26 @@
             return list;
         }
 
-        public static int[] RadixSortAscending(int[] list)
+        public static int[] RadixSortAscending(int[] list) //radix sort works by sorting each digit from least significant digit to most significant digit
         {
             int size = list.Length;
             var maxVal = GetMaxVal(list);
-            for (int exponent = 1; maxVal / exponent > 0; exponent *= 10)
+            for (int exponent = 1; maxVal / exponent > 0; exponent *= 10) //exponent is our number we use to find significant digits
                 CountingSort(list, size, exponent);
 
-            int[] CountingSort(int[] list, int size, int exponent)
+            int[] CountingSort(int[] list, int size, int exponent) //internal function to sort the values
             {
                 var output = new int[size];
                 var occurences = new int[10];
-                for (int i = 0; i < 10; i++)
-                    occurences[i] = 0;
+                for (int i = 0; i < 10; i++) //runs through different conditions to find occurences of values in certain positions
+                { occurences[i] = 0; }
                 for (int i = 0; i < size; i++)
-                    occurences[(list[i] / exponent) % 10]++;
+                { occurences[(list[i] / exponent) % 10]++; }
                 for (int i = 1; i < 10; i++)
-                    occurences[i] += occurences[i - 1];
+                { occurences[i] += occurences[i - 1]; }
                 for (int i = size - 1; i >= 0; i--)
                 {
-                    output[occurences[(list[i] / exponent) % 10] - 1] = list[i];
+                    output[occurences[(list[i] / exponent) % 10] - 1] = list[i]; //copies the values in the set positions in occurences across from the list into output
                     occurences[(list[i] / exponent) % 10]--;
                 }
                 for (int i = 0; i < size; i++)
@@ -99,7 +100,7 @@
                 return list;
             }
 
-            int GetMaxVal(int[] list)
+            int GetMaxVal(int[] list) //finds the largest value in the array
             {
                 int size = list.Length;
                 var maxVal = list[0];
@@ -112,7 +113,7 @@
             return list;
         }
 
-        public static int[] RadixSortDescending(int[] list)
+        public static int[] RadixSortDescending(int[] list) //similar to before however there is a function that has been made to reverse the array that is used at the end of the sort
         {
             int size = list.Length;
             var maxVal = GetMaxVal(list);
@@ -150,18 +151,18 @@
             }
             list = ReverseArray(list);
             return list;
-        } 
+        }
 
-        public static int[] QuickSortAscending(int[] list)
+        public static int[] QuickSortAscending(int[] list) //the array is divided into smaller groups around a PIVOT and then recombined once ordered, in a similar fashion to merge sort
         {
-            int leftIndex = 0;
+            int leftIndex = 0; //setup for the main algorithm as it is run recursively and needs some initial values
             int rightIndex = list.Length - 1;
             QuickSortAlgo(list, leftIndex, rightIndex);
             return list;
 
-            int[] QuickSortAlgo(int[] list, int leftIndex, int rightIndex)
+            int[] QuickSortAlgo(int[] list, int leftIndex, int rightIndex) //main recursive function for the quick sort
             {
-                var i = leftIndex;
+                var i = leftIndex; //declares our limits and our pivot
                 var j = rightIndex;
                 var pivot = list[leftIndex];
                 while (i <= j)
@@ -172,16 +173,16 @@
                     {
                         j--;
                     }
-                    if (i <= j)
+                    if (i <= j) //as long as the left and right indexes haven't crossed over eachother then we continue adding the values back to list
                     {
                         int temp = list[i];
                         list[i] = list[j];
                         list[j] = temp;
-                        i++;
+                        i++; //moves the edges further to continue the search
                         j--;
                     }
                 }
-                if (leftIndex < j)
+                if (leftIndex < j) //if the algorithm hasn't reached the edges of the array then it runs again to seperate the remaining values
                     QuickSortAlgo(list, leftIndex, j);
                 if (i < rightIndex)
                     QuickSortAlgo(list, i, rightIndex);
@@ -189,7 +190,7 @@
             }
         }
 
-        public static int[] QuickSortDescending(int[] list)
+        public static int[] QuickSortDescending(int[] list) //same as ascending but with a reverse function implemented
         {
             int leftIndex = 0;
             int rightIndex = list.Length - 1;
@@ -225,12 +226,12 @@
                 list = ReverseArray(list);
                 return list;
             }
-        } 
+        }
 
-        public static int[] ReverseArray(int[] list)
+        public static int[] ReverseArray(int[] list) //reverses any given array
         {
             int[] reverseList = new int[list.Length];
-            for (int i = 0; i < list.Length; i++)
+            for (int i = 0; i < list.Length; i++) //loops through the whole length of the array assigning the end of one array to the start of another
             {
                 reverseList[i] = list[list.Length - i];
             }
--- CMP1124M A+C AO1/CMP1124M A+C AO1/Testing.cs
+++ CMP1124M A+C AO1/CMP1124M A+C AO1/Testing.cs
@@ -4,32 +4,60 @@
     {
         public Testing()
         {
-            //Functionality 1 - Importing the File Paths
-            string ROAD_1_256_path = @"C:\Users\jacob\OneDrive - University of Lincoln\Desktop\Algorithms & Complexity ASSIGNMENT\Road_1_256.txt";
-            string ROAD_2_256_path = @"C:\Users\jacob\OneDrive - University of Lincoln\Desktop\Algorithms & Complexity ASSIGNMENT\Road_2_256.txt";
-            string ROAD_3_256_path = @"C:\Users\jacob\OneDrive - University of Lincoln\Desktop\Algorithms & Complexity ASSIGNMENT\Road_3_256.txt";
-            string ROAD_1_2048_path = @"C:\Users\jacob\OneDrive - University of Lincoln\Desktop\Algorithms & Complexity ASSIGNMENT\Road_1_2048.txt";
-            string ROAD_2_2048_path = @"C:\Users\jacob\OneDrive - University of Lincoln\Desktop\Algorithms & Complexity ASSIGNMENT\Road_2_2048.txt";
-            string ROAD_3_2048_path = @"C:\Users\jacob\OneDrive - University of Lincoln\Desktop\Algorithms & Complexity ASSIGNMENT\Road_3_2048.txt";
+            while (true)
+            {
+                string ROAD_1_256_path = @"C:\Users\jacob\OneDrive - University of Lincoln\Desktop\Algorithms & Complexity ASSIGNMENT\Road_1_256.txt";
+                string ROAD_2_256_path = @"C:\Users\jacob\OneDrive - University of Lincoln\Desktop\Algorithms & Complexity ASSIGNMENT\Road_2_256.txt";
+                string ROAD_3_256_path = @"C:\Users\jacob\OneDrive - University of Lincoln\Desktop\Algorithms & Complexity ASSIGNMENT\Road_3_256.txt";
+                string ROAD_1_2048_path = @"C:\Users\jacob\OneDrive - University of Lincoln\Desktop\Algorithms & Complexity ASSIGNMENT\Road_1_2048.txt";
+                string ROAD_2_2048_path = @"C:\Users\jacob\OneDrive - University of Lincoln\Desktop\Algorithms & Complexity ASSIGNMENT\Road_2_2048.txt";
+                string ROAD_3_2048_path = @"C:\Users\jacob\OneDrive - University of Lincoln\Desktop\Algorithms & Complexity ASSIGNMENT\Road_3_2048.txt";
+                int[] ROAD_1_256 = FileToArray(ROAD_1_256_path);
+                int[] ROAD_2_256 = FileToArray(ROAD_2_256_path);
+                int[] ROAD_3_256 = FileToArray(ROAD_3_256_path);
+                int[] ROAD_1_2048 = FileToArray(ROAD_1_2048_path);
+                int[] ROAD_2_2048 = FileToArray(ROAD_2_2048_path);
+                int[] ROAD_3_2048 = FileToArray(ROAD_3_2048_path);
 
-            //Functionality 1 - Converting the Files Imported Into Arrays
-            int[] ROAD_1_256 = FileToArray(ROAD_1_256_path);
-            int[] ROAD_2_256 = FileToArray(ROAD_2_256_path);
-            int[] ROAD_3_256 = FileToArray(ROAD_3_256_path);
-            int[] ROAD_1_2048 = FileToArray(ROAD_1_2048_path);
-            int[] ROAD_2_2048 = FileToArray(ROAD_2_2048_path);
-            int[] ROAD_3_2048 = FileToArray(ROAD_3_2048_path);
+                Console.WriteLine();
+                Console.WriteLine("Pick a functionality: ");
+                Console.WriteLine("a) Functionality 1 - Read Files to Arrays");
+                Console.WriteLine("b) Functionality 2 - Display Ascending and Descending Roads (256)");
+                Console.WriteLine("c) Functionality 3 & 4 - Search Array for a value (displays closest value if not found)");
+                Console.WriteLine("d) Functionality 5 - Repeat functionalities 2 & 4 with Roads (2048)");
+                Console.WriteLine("e) Functionality 6 - Repeat functionalities 2 & 4 with Merged Roads (256)");
+                Console.WriteLine("f) Functionality 7 - Repeat functionalities 2 & 4 with Merged Roads (2048)");
+                Console.WriteLine();
+                string answer = Console.ReadLine();
+                answer = answer.ToUpper();
+                switch (answer)
+                {
+                    case "A":
+                        Console.WriteLine("Already Complete!");
+                        break;
+                    case "B":
+                        Functionality2(ROAD_1_256, ROAD_2_256, ROAD_3_256);
+                        break;
+                    case "C":
+                        Console.Write("Pick a value to search for: ");
+                        int value = Convert.ToInt16(Console.ReadLine());
+                        Functionality3(Sorts.BubbleSortAscending(ROAD_1_256), value);
+                        break;
+                    case "D":
+                        Functionality5(ROAD_1_2048, ROAD_2_2048, ROAD_3_2048);
+                        break;
+                    case "E":
+                        Functionality6(ROAD_1_256, ROAD_3_256, 10);
+                        break;
+                    case "F":
+                        Functionality6(ROAD_1_2048, ROAD_3_2048, 50);
+                        break;
+                    default:
+                        Console.WriteLine("Please Enter A Valid Option!");
+                        break;
+                }
 
-            //Functionality2(ROAD_1_256, ROAD_2_256, ROAD_3_256);
-            //Functionality3(Sorts.BubbleSortAscending(ROAD_1_256), 2000);
-            //Functionality4 IMPLEMENTED during functionality3
-            Functionality5(ROAD_1_2048, ROAD_2_2048, ROAD_3_2048);
-
-
-
-            Console.WriteLine();
-            Console.WriteLine("Press any key to exit.");
-            System.Console.ReadKey();
+            }
         }
 
         public static void Functionality2(int[] list1, int[] list2, int[] list3)
@@ -82,6 +110,26 @@
             PrintList(Sorts.BubbleSortAscending(list3), 50);
             Console.WriteLine("DESCENDING:");
             PrintList(Sorts.BubbleSortDescending(list3), 50);
+
+            Console.Write("Pick a value to search for in ROAD_3_2048: ");
+            int key = Convert.ToInt16(Console.ReadLine());
+            list3 = Sorts.RadixSortAscending(list3);
+            PrintList(Search.LinearSearch(list3, key, true), 1);
+        }
+
+        public static void Functionality6(int[] list1, int[] list2, int increment)
+        {
+            int[] NEW_ROAD = MergeRoads(list1, list2);
+            Console.WriteLine("NEW ROAD:");
+            Console.WriteLine("ASCENDING:");
+            PrintList(Sorts.QuickSortAscending(NEW_ROAD), increment);
+            Console.WriteLine("DESCENDING:");
+            PrintList(Sorts.InsertionSortDescending(NEW_ROAD), increment);
+
+            Console.Write("Pick a value to search for: ");
+            int key = Convert.ToInt16(Console.ReadLine());
+            NEW_ROAD = Sorts.RadixSortAscending(NEW_ROAD);
+            PrintList(Search.LinearSearch(NEW_ROAD, key, true), 1);
         }
 
         public static int[] FileToArray(string filePath)
